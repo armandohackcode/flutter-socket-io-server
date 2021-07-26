1df698ee18978e3d442371521515885b96fe0a4c
armandohackcode <jlopezrodriguez687@gmail.com>
2021-07-26 15:11:29 -0400

    inicializando proyecto base para socket server

---
+++ sockets/socket.js
@@ -0,0 +1,13 @@
+// mensajes de sockets
+const {io} = require('../index');
+io.on('connection', client => {
+    console.log('Cliente conectado');
+    client.on('disconnect', () => { 
+        console.log('Cliente desconectado');
+     });
+
+     client.on('mensaje',(payload)=>{
+         console.log('Mensaje',payload);
+     });
+     io.emit('mensaje',{admin:'Hola ARMANDO'});
+  });
